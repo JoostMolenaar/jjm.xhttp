ed36d0812b9ccc4054d743c606edf823d5e7de7a
Joost Molenaar <j.j.molenaar@gmail.com>
2015-05-09 21:31:01 +0200

    Switch versioning scheme

--- setup.py
+++ setup.py
@@ -16,11 +16,11 @@ def read_file(filename):
 setup(
     name='xhttp',
     version=read_file('xhttp.egg-info/version.txt'),
-    version_command=('git describe', 'pep440-git-dev'),
-    packages=['xhttp'],
+    version_command=('git describe', 'pep440-git'),
+    url='https://github.com/j0057/xhttp',
     author='Joost Molenaar',
     author_email='j.j.molenaar@gmail.com',
-    url='https://github.com/j0057/xhttp',
+    packages=['xhttp'],
     data_files=[ (root, [ root + '/' + fn for fn in files ])
                  for src_dir in static_dirs
                  for (root, dirs, files) in os.walk(src_dir) ],
